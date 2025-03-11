9542d64b352f9ecef4dd639e9de42ed6a2c4e61b
AbhinaySingh15 <abhinaysingh640@gmail.com>
2025-03-11 12:30:26 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+   const accountId = 12345
+   let accountEmail = "abhinay@gmail.com"
+   var accountPassword = "54321"
+   accountCity ="bhopal"
+   let accountState ;
+
+   //accountId = 2// not allowed , b/c const refered to consant things.
+
+   // for defining variables we use let and var. but we use more let., never use var .
+
+   accountEmail = "om@gmail.com"
+   accountPassword ="9876002"
+   accountCity = "satna"
+
+   console.log(accountId) ;
+
+   console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "22abs"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/*
+"22" => 33
+"22abs" => NaN
+true => 1 ; false => 0
+*/
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=> true ; 0 => false
+// "" => false
+// "Abhinay" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
