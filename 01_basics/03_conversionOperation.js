fb9ea834cadc5b158a8f3ef3cfba10cc7c9e5fb5
AbhinaySingh15 <abhinaysingh640@gmail.com>
2025-03-13 10:36:36 +0000

    done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,3 +27,34 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+//**************  Operation *****************//
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(1/3);
+console.log(2%3);
+*/
+
+let str1 = "hello"
+let str2 = " Abhinay"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 +2);
+console.log(1 + 2 + "2")
+
+console.log( (3 + 4) * 5 % 3); // aesa likho
+
+console.log (+true);
+console.log(+"");  // ye preffered nhi hai
