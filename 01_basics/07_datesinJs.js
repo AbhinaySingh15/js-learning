cb70c183b0760bbb499fb2ada1a17c5ddda2788c
Abhinay Singh <abhinaysingh640@gmail.com>
2025-12-06 16:51:27 +0000

    dates done

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -14,3 +14,21 @@ console.log(typeof myDate);
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
 // console.log(myCreatedDate.toLocaleString());
 
+ let myCreatedDate = new Date("2023-01-14")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime()); // it is showing in milisecond from the starting.
+
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay()); // other methods of date.
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+})
