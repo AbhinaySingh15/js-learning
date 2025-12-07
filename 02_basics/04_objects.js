997ab8bc94813e74d65aa631ed03c93f40b670db
Abhinay Singh <abhinaysingh640@gmail.com>
2025-12-07 12:42:27 +0000

    objects done

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -21,3 +21,68 @@ const regularUser = {
 
 console.log(regularUser.fullname.userfullname.firstName);
 
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = {obj1, obj2}
+//const obj3 = Object.assign({}, obj1, obj2)
+
+const Obj3 = {...obj1, ...obj2} //we use it more oftenly
+console.log(Obj3);
+
+
+const user = [
+    
+    {
+        id: 1,
+        email: "aaaa@milgaya"
+    },
+    {
+        id: 1,
+        email: "aaaa@milgaya"
+    },
+    {
+        id: 1,
+        email: "aaaa@milgaya"
+    },
+    {
+        id: 1,
+        email: "aaaa@milgaya"
+    },
+]
+
+user[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+console.log(tinderUser.hasOwnProperty('isLogged')); // one of the methods.
+
+const course = {
+    coursename: "js in hindi",
+    price: "9999",
+    courseInstructor: "Hitesh Sir"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course // destructing object
+
+//console.log(courseInstructor);
+console.log(instructor);
+
+
+// {
+//     "name": "histesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+{
+    {},
+    {},
+    {}
+}
