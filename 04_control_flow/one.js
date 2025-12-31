e519b5b844c67ddce861309dc7a4ce3e8b610c02
Abhinay Singh <abhinaysingh640@gmail.com>
2025-12-31 18:02:48 +0000

    control flow done

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -3,9 +3,9 @@ const isUserloggedIn = true
 const temperature = 41
 
 if (temperature === 41){
-    console.log("less than 50");
+    // console.log("less than 50");
 } else{
-    console.log("temperature is greater than 50");
+    // console.log("temperature is greater than 50");
 }
 
 /**<, >, <=, >=, ==, !=, ===(checking dataType also), !== */
@@ -15,10 +15,10 @@ const score = 200
 
 if (score > 100) {
     let power = "fly" /**if we use var instead of const and let it wil allow to excess as global . which is not correct. */
-    console.log(`User power: ${power}`);
+    // console.log(`User power: ${power}`);
 }
 
-console.log(`User power: ${power}`);
+//console.log(`User power: ${power}`);
 
 
 const balance = 1000
@@ -26,3 +26,30 @@ const balance = 1000
 if (balance > 500) console.log("test");
 /**Here scope of if is considered.it is known as implicit scope.and it is only take command that are written on that line only.it is not a good code. */
 
+
+
+
+if (balance < 500){
+    // console.log("less than 500");
+} else if (balance < 750){
+    // console.log("less than 750");
+}else if(balance < 900){
+    // console.log("less than 900")
+}else{
+    // console.log("less than 1200")
+}
+
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitCard){
+    console.log("Allow to buy course");
+}
+
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("User logged in");
+}
